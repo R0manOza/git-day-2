188fe3c20d62d1fd6b09891bc5fcd22623ccaa5a
R0manOza <161681342+R0manOza@users.noreply.github.com>
2026-07-08 01:39:05 +0400

    extra extra

---
+++ net/KiuFinalExam1/KiuFinalExam1/Controllers/securityController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace KiuFinalExam1.Controllers
+{
+    public class securityController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        static Dictionary<String, String> users = new Dictionary<string, string>()
+        {
+            {"Mike" , "1234" },
+            {"jake" , "1234" }
+        };
+        [HttpPost]
+        public IActionResult Login(String UID , String PWD)
+        {
+            if (users.ContainsKey(UID) && users[UID]==PWD)
+
+            {
+                var claims = new List<Claim> { new Claim(ClaimTypes.Name, UID) };
+                var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimIdentity)).Wait();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Msg = "incorrect credentials";
+                return Login();
+            }
+               
+        }
+    }
+}
